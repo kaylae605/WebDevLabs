ef39fcc7b7c4b52d32c553304f66692f9627ab3a
Kayla Edwards <kaylae@andrew.cmu.edu>
2025-03-17 10:32:32 -0400

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -1,27 +1,28 @@
-var x = 5;
-var y = 7;
-var z = x + y; 
-console.log(z); 
-var A = "Hello"; 
-var B = "world!"; 
-var C = A + B; 
-console.log(C); 
+// var x = 5;
+// var y = 7;
+// var z = x + y; 
+// console.log(z); 
+// var A = "Hello"; 
+// var B = "world!"; 
+// var C = A + B; 
+// console.log(C); 
 
-SumNPrint(x,y);
-SumNPrint(A,B); 
+// SumNPrint(x,y);
+// SumNPrint(A,B); 
 
-if (C.length > z){
-    console.log(C); 
-}else if (C.length < z) {
-    console.log(z); 
-}else {
-    console.log("good job!"); 
-}
+// if (C.length > z){
+//     console.log(C); 
+// }else if (C.length < z) {
+//     console.log(z); 
+// }else {
+//     console.log("good job!"); 
+// }
 
-let L1 = ["Watermelon", "Pineapple", "Pear", "Banana"];
-let L2 = ["Apple", "Banana", "Kiwi", "Orange"]; 
+// let L1 = ["Watermelon", "Pineapple", "Pear", "Banana"];
+// let L2 = ["Apple", "Banana", "Kiwi", "Orange"]; 
 
-findTheBanana(L1,L2); 
+// findTheBanana(L1,L2); 
+greetingFunc("Kayla"); 
 
 document.addEventListener('DOMContentLoaded', function() {
     if (window.location.href.endsWith("index.html")) {
@@ -30,10 +31,10 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-function SumNPrint(x1, x2){
-    let x3 = x1+x2; 
-    console.log(x3); 
-}
+// function SumNPrint(x1, x2){
+//     let x3 = x1+x2; 
+//     console.log(x3); 
+// }
 
 // function findTheBanana(arr){
 //     for(i = 0; i < arr.length; i++){
@@ -44,19 +45,19 @@ function SumNPrint(x1, x2){
 //     }
 // }
 
-function findTheBanana(arr1,arr2){
-    if (arr1.forEach(isBanana) == 1){
-        console.log("We found a banana in the first array")
-    } else if (arr2.forEach(isBanana) == 1){
-        console.log("We found a banana in the second array")
-    }
-}
+// function findTheBanana(arr1,arr2){
+//     if (arr1.forEach(isBanana) == 1){
+//         console.log("We found a banana in the first array")
+//     } else if (arr2.forEach(isBanana) == 1){
+//         console.log("We found a banana in the second array")
+//     }
+// }
 
-function isBanana(str){
-    if (str.localeCompare("Banana") == 0){
-        return 1
-    }
-}
+// function isBanana(str){
+//     if (str.localeCompare("Banana") == 0){
+//         return 1
+//     }
+// }
 
 
 
@@ -100,12 +101,30 @@ function showList(){
   }
 }
 
+
+
+
+
+function validate() {
+    var userName = document.getElementById("fname");
+    var userEmail = document.getElementById("email");
+    var userComment = document.getElementById("comment");
+    var message = document.getElementById("errMessage");
+    if (!userName.checkValidity() || !userEmail.checkValidity() || !userComment.checkValidity()){
+        message.innerHTML = "please fill out the form correctly so I can get back to you"; 
+    }
+
+}
+
+
+
 /*I asked gemini for help writing the bottom two functions because I'm not entirely too
 familiar with JavaScript API
 
 I asked to help format it based on the specs given and explain so I could understand the implementation.
 If needed I can explain how it works so you know I'm not just mindlessly copy pasting. 
 */
+
 
 $(document).ready(function() {
     $('#readMore').click(function() {
@@ -119,43 +138,9 @@ $(document).ready(function() {
     });
 });
 
-document.getElementById('contact').addEventListener('submit', function(event) {
-    let nameInput = document.getElementById('name');
-    let emailInput = document.getElementById('email');
-    let commentInput = document.getElementById('comment');
-
-    let nameError = document.getElementById('nameError');
-    let emailError = document.getElementById('emailError');
-    let commentError = document.getElementById('commentError');
-   
-
-    nameError.textContent = "";
-    emailError.textContent = "";
-    commentError.textContent = "";
 
 
 
-    if (!nameInput.checkValidity()) {
-        nameError.textContent = "Please fill out this field.";
-        hasErrors = true;
-        
-    }
-
-    if (!emailInput.checkValidity()) {
-        emailError.textContent = "Please fill out this field.";
-        hasErrors = true;
-
-    }
-
-    if (!commentInput.checkValidity()) {
-        commentError.textContent = "Please fill out this field.";
-        hasErrors = true;
-      
-    }
-
-   
-
-});
 
 
 
